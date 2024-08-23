4b0ec556c695d20a1c233e001fe1ca696b40e9b8
Amna <142903458+amnaa26@users.noreply.github.com>
2024-08-23 20:06:06 +0500

    Update q7.py

--- Labs/01/q7.py
+++ Labs/01/q7.py
@@ -3,6 +3,10 @@
 # Q7) Write a program that accepts a word from the user and reverses it using loop. For example, ‘Pakistan’ becomes‘natsikaP’.
 
 user = input("Enter a word: ")
-reversed_word = user[::-1] #string[beginning:end:step] -> string slicing
-print("Original word: ", user)
-print("Reversed word: ", reversed_word)
+reversed_word = []
+for i in range(len(user)-1, -1, -1): #range(start, stop, step)
+    reversed_word.append(user[i])
+
+reversed_str = ""
+print("Orginal word: ", user)
+print("Reversed word: ", reversed_str.join(reversed_word))

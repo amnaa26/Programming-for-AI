3ac38d1c26c502fd41e3afeec0ba9829e27dedd8
Amna <142903458+amnaa26@users.noreply.github.com>
2024-09-07 16:36:31 +0500

    added header

--- Labs/03/q1.py
+++ Labs/03/q1.py
@@ -1,3 +1,11 @@
+'''
+ Programmer: Amna(23k-0066)
+ Date: 4/Sept/2024
+ Q1) Write a Python program that reads a text file, counts the number of characters in it, andprints the total word count. Handle all possible exceptions as well.
+ 
+'''
+
+
 try:
     with open('text.txt', 'r') as text_file:
         content = text_file.read()

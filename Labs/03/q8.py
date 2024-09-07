492e8a6316f5788763e8339e356d6ce9b6b850dc
Amna <142903458+amnaa26@users.noreply.github.com>
2024-09-07 16:43:35 +0500

    added header

--- Labs/03/q8.py
+++ Labs/03/q8.py
@@ -1,3 +1,10 @@
+'''
+ Programmer: Amna(23k-0066)
+ Date: 4/Sept/2024
+ Q8) Take two integer numbers from user as input. Divide these two numbers. If one number is being divided by zero (another number) then handle ZeroDivisionError and if entered value
+     is wrong (for example, a string) then handle ValueError.
+'''
+
 try:
     value1 = float(input("Enter value 1: "))
     value2 = float(input("Enter value 2: "))

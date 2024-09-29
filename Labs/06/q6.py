da71d4313c6a21e9224232922d308fe678f063ca
Amna <142903458+amnaa26@users.noreply.github.com>
2024-09-29 12:07:27 +0500

    added question

--- Labs/06/q6.py
+++ Labs/06/q6.py
@@ -1,5 +1,9 @@
-#We are working on the movies data set andnot on alcohol data set.. 
-#So we will print the details of the movies realesd in the years from 1987 and 1989
+'''
+Write a Pandas program to find out the alcohol consumption details in the year '1987' or '1989' from the world alcohol consumption dataset.
+'''
+
+#We are working on the movies data set and not on alcohol data set.. 
+#So we will print the details of the movies released in the years from 1987 and 1989
 
 import pandas as pd
 
